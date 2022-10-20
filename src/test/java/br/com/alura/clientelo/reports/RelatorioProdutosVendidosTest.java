1aaec31adc23cbf232728babe57a9acae434b5d1
victor.pongolino <victor.pongolino@hotmail.com>
2022-10-19 23:28:54 -0300

    [SEMANA-03] [WIP] Testes unitários

---
+++ src/test/java/br/com/alura/clientelo/reports/RelatorioProdutosVendidosTest.java
@@ -0,0 +1,44 @@
+package br.com.alura.clientelo.reports;
+
+import br.com.alura.clientelo.Pedido;
+import br.com.alura.clientelo.ProcessadorDeCsv;
+import br.com.alura.clientelo.reports.logic.OrdenacaoPedido;
+import br.com.alura.clientelo.reports.logic.OrdenacaoPedidoImpl;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class RelatorioProdutosVendidosTest {
+
+    private OrdenacaoPedidoImpl ordenacaoPedidoImpl = new OrdenacaoPedidoImpl();
+
+    @Test
+    public void retornarCategoria() {
+        List<Pedido> pedidos = ordenacaoPedidoImpl.ordenarQuantidade(obterRelatorios());
+        Pedido p1 = pedidos.get(0);
+        Pedido p2 = pedidos.get(1);
+        Pedido p3 = pedidos.get(2);
+
+        assertTrue(p1.getProduto().equals("iPhone 13 Pro") && p1.getQuantidade() == 6 && p1.getCategoria().equals("CELULARES"));
+        assertTrue(p2.getProduto().equals("Galaxy S22 Ultra") && p2.getQuantidade() == 5 && p2.getCategoria().equals("CELULARES"));
+        assertTrue(p3.getProduto().equals("Galaxy Tab S8") && p3.getQuantidade() == 4 && p3.getCategoria().equals("INFORMÁTICA"));
+    }
+
+    @Test
+    public void retornarCategoriaVazia() {
+        List<Pedido> pedidos = ordenacaoPedidoImpl.ordenarQuantidade(Collections.emptyList());
+        assertTrue(pedidos.isEmpty());
+    }
+
+    private List<Pedido> obterRelatorios() {
+        return Arrays.asList(ProcessadorDeCsv.processaArquivo("pedidos.csv"));
+    }
+}
---
+++ src/test/java/br/com/alura/clientelo/reports/RelatorioVendasCategoriaTest.java
@@ -0,0 +1,35 @@
+package br.com.alura.clientelo.reports;
+
+import br.com.alura.clientelo.Pedido;
+import br.com.alura.clientelo.ProcessadorDeCsv;
+import br.com.alura.clientelo.reports.logic.OrdenacaoPedidoImpl;
+import br.com.alura.clientelo.reports.logic.QuantidadeCategoria;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+
+public class RelatorioVendasCategoriaTest {
+
+    private OrdenacaoPedidoImpl ordenacaoPedidoImpl = new OrdenacaoPedidoImpl();
+
+    @Test
+    public void estaNaOrdemVendasCategorias() {
+        Map<String, QuantidadeCategoria> produtos = ordenacaoPedidoImpl.ordenarPorCategoria(obterRelatorios());
+
+        Integer[] quantidadeCategorias = produtos.values().stream().map(QuantidadeCategoria::getQuantidade).limit(5).collect(Collectors.toList()).toArray(new Integer[0]);
+        BigDecimal[] precos = produtos.values().stream().map(QuantidadeCategoria::getMontante).limit(5).collect(Collectors.toList()).toArray(new BigDecimal[0]);
+
+        // TODO: Mudar para JUNIT 5
+        assertEquals(precos, new BigDecimal[]{ new BigDecimal("1507.64"), new BigDecimal("1987.97"), new BigDecimal("12378.98"), new BigDecimal("97801.50"), new BigDecimal("64698.40")});
+        assertEquals(quantidadeCategorias, new Integer[]{ 9, 2, 4, 11, 9});
+        assertEquals(Arrays.copyOfRange(produtos.keySet().toArray(), 0, 5), new String[]{ "LIVROS", "AUTOMOTIVA", "MÓVEIS", "CELULARES", "INFORMÁTICA" });
+    }
+
+    private List<Pedido> obterRelatorios() {
+        return Arrays.asList(ProcessadorDeCsv.processaArquivo("pedidos.csv"));
+    }
+}
