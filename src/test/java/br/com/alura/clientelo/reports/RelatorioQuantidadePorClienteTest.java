cffe6de31b67cc85d1847fc169a7b97843884d7b
victor.pongolino <victor.pongolino@hotmail.com>
2022-10-26 23:05:17 -0300

    [SEMANA-03] Adiciona teste para cliente fiel

---
+++ src/test/java/br/com/alura/clientelo/reports/RelatorioQuantidadePorClienteTest.java
@@ -0,0 +1,27 @@
+package br.com.alura.clientelo.reports;
+
+import br.com.alura.clientelo.reports.logic.OrdenacaoPedidoImpl;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class RelatorioQuantidadePorClienteTest {
+
+    private static final String NOME_CLIENTE_UNICO = "ANA";
+    private final OrdenacaoPedidoImpl ordenacaoPedidoImpl = new OrdenacaoPedidoImpl();
+
+    @Test
+    public void validarClientesFieisSeRetornaApenasUmCliente() {
+        Map<String, Integer> clientesFieis = ordenacaoPedidoImpl.ordenarPorClientes(PedidoFake.getPedidos());
+        Map.Entry<String, Integer> cliente = clientesFieis.entrySet().stream()
+                .filter(nomeCliente -> nomeCliente.getKey().equals(NOME_CLIENTE_UNICO))
+                .limit(3)
+                .findFirst().get();
+
+        assertEquals(NOME_CLIENTE_UNICO, cliente.getKey());
+        assertEquals(4, cliente.getValue());
+    }
+
+}
