d5a1cdc7857469099a012f34e3fcf4a3bca4bab5
victor.pongolino <victor.pongolino@hotmail.com>
2022-12-14 23:47:39 -0300

    [SEMANA-09] [WIP] Conflito Swagger

--- src/main/java/br/com/alura/clientelo/config/SwaggerConfig.java
+++ src/main/java/br/com/alura/clientelo/config/SwaggerConfig.java
@@ -1,13 +1,12 @@
 package br.com.alura.clientelo.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -21,8 +20,9 @@ import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Configuration
+@EnableWebMvc
 @EnableSwagger2
+@Component
 public class SwaggerConfig {
 
     private List<ResponseMessage> responseMessageForGET()
@@ -34,8 +34,8 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("br.com.alura.clientelo.controller.categoria"))
-                .paths(PathSelectors.ant("/api/*"))
+                .apis(RequestHandlerSelectors.basePackage("br.com.alura.clientelo.controller"))
+                .paths(PathSelectors.any())
                 .build()
                 .useDefaultResponseMessages(false)
                 .globalResponseMessage(RequestMethod.GET, responseMessageForGET())
