9a5a820492e03754b47404150d2525b6c9f1748b
victor.pongolino <victor.pongolino@hotmail.com>
2022-12-18 22:41:49 -0300

    [SEMANA-10] Add teste unitário para listar um pedido por ID

--- src/test/java/br/com/alura/clientelo/controller/pedido/PedidoControllerTest.java
+++ src/test/java/br/com/alura/clientelo/controller/pedido/PedidoControllerTest.java
@@ -1,14 +1,16 @@
 package br.com.alura.clientelo.controller.pedido;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -22,10 +24,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import br.com.alura.clientelo.controller.pedido.dto.CadastroPedidoResultado;
 import br.com.alura.clientelo.controller.pedido.dto.CriarPedidoDTO;
@@ -52,19 +51,16 @@ class PedidoControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @Autowired
+    @MockBean
     private PedidoService pedidoService;
 
     @Test
     public void deveRetornar201_consultarPedidoSendoVendedorCerto() throws Exception {
-        Categoria categoria = new Categoria("CATEGORIA", Categoria.Status.ATIVA);
-        Endereco endereco = new Endereco("RUA", "NUMERO", "COMPLEMENTO", "BAIRRO", "CIDADE", "ESTADO");
-        Produto produto = new Produto("Nome Produto", "DESC", 12, new BigDecimal("2.0"), categoria);
-        Cliente cliente = new Cliente("NOME", "CPF", "TELEFONE", endereco);
+        Pedido pedido = gerarPedidoMockado();
+        pedido.setId(1L);
 
-        List<ItemPedido> itemPedido = new ArrayList<>();
-        Pedido pedido = new Pedido(itemPedido, cliente, LocalDate.of(2022, 12, 15));
-        itemPedido.add(new ItemPedido(new BigDecimal("1.0"), 1, pedido, produto, BigDecimal.ZERO, ItemPedido.TipoDesconto.NENHUM));
+        Mockito.when(pedidoService.cadastrar(pedido)).thenReturn(pedido);
+        Mockito.when(pedidoService.findById(1L)).thenReturn(Optional.of(pedido));
 
         Pedido ultimoPedido = pedidoService.cadastrar(pedido);
 
@@ -74,10 +70,40 @@ class PedidoControllerTest {
                 .andReturn();
 
         DetalhePedidoVO detalhePedidoVO = objectMapper.readValue(resultado.getResponse().getContentAsString(), DetalhePedidoVO.class);
-        assertTrue(detalhePedidoVO.getDetalheProduto().size() == 1);
+        assertEquals(1, detalhePedidoVO.getDetalheProduto().size());
         assertEquals(pedido.getPrecoFinal(), detalhePedidoVO.getValor());
         assertEquals(pedido.getDescontoTotal(), detalhePedidoVO.getDesconto());
         assertEquals(pedido.getData(), detalhePedidoVO.getDataPedido());
+    }
+
+    @Test
+    public void deveRetornar404_QuandoIdDoProdutoNaoExiste() throws Exception {
+        Pedido pedido = gerarPedidoMockado();
+        pedido.setId(1L);
+
+        Mockito.when(pedidoService.cadastrar(pedido)).thenReturn(pedido);
+        Mockito.when(pedidoService.findById(1L)).thenReturn(Optional.empty());
+
+        Pedido ultimoPedido = pedidoService.cadastrar(pedido);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/pedidos/{id}", ultimoPedido.getId())
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message", equalTo("Pedido ID 1 não existe!")));
+    }
+
+    private Pedido gerarPedidoMockado() {
+        Categoria categoria = new Categoria("CATEGORIA", Categoria.Status.ATIVA);
+        Endereco endereco = new Endereco("RUA", "NUMERO", "COMPLEMENTO", "BAIRRO", "CIDADE", "ESTADO");
+        Produto produto = new Produto("Nome Produto", "DESC", 12, new BigDecimal("2.0"), categoria);
+        Cliente cliente = new Cliente("NOME", "CPF", "TELEFONE", endereco);
+
+        List<ItemPedido> itemPedido = new ArrayList<>();
+
+        Pedido pedido = new Pedido(itemPedido, cliente, LocalDate.of(2022, 12, 15));
+
+        itemPedido.add(new ItemPedido(new BigDecimal("1.0"), 1, pedido, produto, BigDecimal.ZERO, ItemPedido.TipoDesconto.NENHUM));
+        return pedido;
     }
 
     @Test
